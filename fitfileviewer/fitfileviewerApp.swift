499cc667e4ca34e419b9f3bc236aacb9ced3a7a9
Roberto Viola <Cagnulein@gmail.com>
2024-03-14 18:49:31 +0100

    Initial Commit

---
+++ fitfileviewer/fitfileviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitfileviewerApp.swift
+//  fitfileviewer
+//
+//  Created by Roberto Viola on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct fitfileviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
